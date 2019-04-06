8ad74598f2212140905076aadb058ed1b00ab524
Dmitry Manannikov <slonoed@uber.com>
2019-04-06 19:34:15 +0200

    Update to latest jsref

--- jsref.rb
+++ jsref.rb
@@ -3,9 +3,9 @@ require "language/node"
 class Jsref < Formula
   desc "JavaScript refactoring language server"
   homepage "https://github.com/slonoed/jsref#readme"
-  url "https://registry.npmjs.org/@slonoed/jsref/-/jsref-1.0.2.tgz"
-  version "1.0.2"
-  sha256 "1e4eed7d1d9479f67de2448723e4d0497a18537d9c67b2a3fe194f9312e00ae7"
+  url "https://registry.npmjs.org/@slonoed/jsref/-/jsref-1.0.3.tgz"
+  version "1.0.3"
+  sha256 "848663d2a1e22122bcbd61bd5d0c221a188b0b7fc6b3f348bcfe2b3d470e0648"
 
   depends_on "node"
 
